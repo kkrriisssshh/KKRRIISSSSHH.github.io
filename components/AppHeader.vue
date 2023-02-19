25f2a12330d64a81d445f09a391f74479a35a753
kkrriisssshh <jamunasittampalam@gmail.com>
2023-02-19 12:15:38 +0100

    v5

---
+++ components/AppHeader.vue
@@ -0,0 +1,31 @@
+<style lang="css">
+    @import url(https://igoradamenko.github.io/awsm.css/css/awsm_theme_black.min.css);
+</style>
+<template>
+    <div>
+        <header>
+            <h1>
+                [krissh] 🐈 🐅
+            </h1>
+            <p>
+                [the most intelligent human on earth]
+            </p>
+            <nav>
+              <ul>
+                <li>
+                  <NuxtLink to="/">[homepage]</NuxtLink>
+                </li>
+                <li>
+                  <NuxtLink to="/projects">[projects]</NuxtLink>
+                </li>
+                <li>
+                  <NuxtLink to="/about-me">[about-me]</NuxtLink>
+                </li>
+                <li>
+                  <NuxtLink to="/contact-me">[contact-me]</NuxtLink>
+                </li>
+              </ul>
+            </nav>
+        </header>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <AppHeader></AppHeader>
+        <main>
+            <img style="opacity: 60%;" src="https://thumbs.gfycat.com/NaturalCelebratedDutchshepherddog-size_restricted.gif" alt="anime sky">
+            <h5>
+                welcome to my homepage
+            </h5>
+            <p>
+                this is where i post things that interest me and things i'm doing at the moment
+            </p>
+        </main>
+    </div>
+</template>
